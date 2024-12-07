e7c1faf386237f7a82d8df2aabe3ae845bc6ce91
migao2006 <a0912647176@gmail.com>
2024-12-08 05:06:37 +0800

    Add files via upload

---
+++ api/merge-pdf.js
@@ -0,0 +1,23 @@
+import formidable from "formidable";
+import fs from "fs";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    const form = new formidable.IncomingForm();
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        res.status(500).send("解析文件失敗");
+        return;
+      }
+      res.status(200).send("PDF 已成功合併（功能待實現）");
+    });
+  } else {
+    res.status(405).send("方法不被允許");
+  }
+}

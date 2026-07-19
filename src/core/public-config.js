af0781f851444313765e1bba852e03b7e946b42d
migao2006 <migao2006@users.noreply.github.com>
2026-07-19 21:15:48 +0800

    Connect production prediction API

--- src/core/public-config.js
+++ src/core/public-config.js
@@ -6,7 +6,7 @@ export const publicConfig = Object.freeze({
   supabaseUrl: "https://zuhwkxlmnvwiktcmijup.supabase.co",
   supabasePublishableKey: "sb_publishable_4T3QrbPrb0ZNzXceEYUqig_oWUpaoHd",
   authConfirmationRedirectUrl: runtimeBaseUrl,
-  predictionApiBaseUrl: "",
+  predictionApiBaseUrl: "https://zuhwkxlmnvwiktcmijup.supabase.co/functions/v1/",
   predictionApiTimeoutMs: 12_000,
   predictionApiContractVersion: "prediction-snapshot.v1",
 });
